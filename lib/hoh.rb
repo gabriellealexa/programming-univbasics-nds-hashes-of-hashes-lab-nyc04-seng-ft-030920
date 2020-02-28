ab56be1baa6f96390464e7cd2feb8cb2d2231f73
Gabrielle Noel <gabby@gabriellealexa.com>
2020-02-28 07:58:33 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -32,7 +32,13 @@ PORTION_3 = {
 
 def naming_system
   {
-    [PORTION_1, PORTION_2, PORTION_3, PORTION_4]
+    PORTION_1,
+    sub_category: { PORTION_2,
+    sub_category: { PORTION_3,
+    sub_category: {PORTION_4
+  }
+    }
+    }
   }
   
   
